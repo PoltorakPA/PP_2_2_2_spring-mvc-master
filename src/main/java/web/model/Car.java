536cabc91879ccc1e859b190eeedb13c177365a6
PoltorakPA <paulepoltorak.com>
2023-01-10 20:38:40 +0500

    Initial commit

---
+++ src/main/java/web/model/Car.java
@@ -0,0 +1,64 @@
+package web.model;
+
+import java.util.Objects;
+
+public class Car {
+    private String color;
+    private String model;
+    private int year;
+    public Car(){
+
+    }
+
+    public Car(String color, String model, int year) {
+        this.color = color;
+        this.model = model;
+        this.year = year;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Car car = (Car) o;
+        return year == car.year && Objects.equals(color, car.color) && Objects.equals(model, car.model);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(color, model, year);
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "color='" + color + '\'' +
+                ", model='" + model + '\'' +
+                ", year=" + year +
+                '}';
+    }
+}
---
+++ src/main/java/web/service/CarServiceImpl.java
@@ -0,0 +1,29 @@
+package web.service;
+
+import org.springframework.stereotype.Component;
+import web.model.Car;
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class CarServiceImpl implements CarService {
+    private List<Car> cars = new ArrayList<>();
+
+    {
+        cars.add(new Car("Black", "BMW", 2022));
+        cars.add(new Car("White", "Нива", 1988));
+        cars.add(new Car("Grey", "Skoda", 2015));
+        cars.add(new Car("Blue", "Audi", 2021));
+        cars.add(new Car("Black", "Jeep", 2018));
+    }
+
+    @Override
+    public List<Car> getAllCars() {
+        return cars;
+    }
+
+    @Override
+    public List<Car> getCarList(int count) {
+        return (count >= 0) ? cars.subList(0, Math.min(count, cars.size())) : cars;
+    }
+}
